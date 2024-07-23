9d6744f277f8c738637b3692807b63f1b5888a0d
Y0S0NG <yosongyxp@gmail.com>
2024-07-23 13:04:56 +0800

    在Question的结构中新增了辨别多选题的字段

--- quiz/models.py
+++ quiz/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 
 class Question(models.Model):
     text = models.TextField()
+    is_multiple_choice = models.BooleanField(default=False)  # 新增字段
+
 
     def __str__(self):
         return self.text
